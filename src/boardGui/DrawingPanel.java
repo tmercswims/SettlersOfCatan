dc7ccde338fada1da6aedd4a32ab96b50385827a
sbreslow <samuel_breslow@brown.edu>
2014-03-31 15:02:35 -0400

    created java project

---
+++ src/boardGui/DrawingPanel.java
@@ -0,0 +1,13 @@
+package boardGui;
+
+public class DrawingPanel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/boardGui/Tile.java
@@ -0,0 +1,5 @@
+package boardGui;
+
+public class Tile {
+
+}
