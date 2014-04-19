68dc96aea285c6fb47b143bf46a5a29e08e7a30b
atreil <alexander_treil@brown.edu>
2014-04-19 16:57:45 -0400

    Fixed null pointer issues in Client

--- src/edu/brown/cs032/atreil/catan/networking/client/CatanClient.java
+++ src/edu/brown/cs032/atreil/catan/networking/client/CatanClient.java
@@ -114,13 +114,13 @@ public class CatanClient extends Thread{
 			synchronized(_hasUpdatedBoard){
 				_hasUpdatedBoard = true;
 				_updatedBoard = (Board) packet.getObject();
-				_updatedBoard.notifyAll();
+				_hasUpdatedBoard.notifyAll();
 			}
 		} else if(type == Packet.PLAYERARRAY){
 			synchronized(_hasUpdatedPlayers){
 				_hasUpdatedPlayers = true;
 				_updatedPlayers = (Player[]) packet.getObject();
-				_updatedPlayers.notifyAll();
+				_hasUpdatedPlayers.notifyAll();
 			}
 		} else if(type == Packet.START){
 			System.out.println("Start your turn");
@@ -231,7 +231,7 @@ public class CatanClient extends Thread{
 		synchronized (_hasUpdatedPlayers) {
 			while(_hasUpdatedPlayers.equals(false)){
 				try {
-					_updatedPlayers.wait();
+					_hasUpdatedPlayers.wait();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -253,7 +253,7 @@ public class CatanClient extends Thread{
 		synchronized (_hasUpdatedBoard) {
 			while(_hasUpdatedBoard.equals(false)){
 				try {
-					_updatedBoard.wait();
+					_hasUpdatedBoard.wait();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
