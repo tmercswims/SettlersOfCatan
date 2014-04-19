39f638e47f55c4127a0c745b55c3fb729b95bb92
atreil <alexander_treil@brown.edu>
2014-04-19 14:45:54 -0400

    Updated error protocol

--- src/edu/brown/cs032/atreil/catan/networking/client/CatanClient.java
+++ src/edu/brown/cs032/atreil/catan/networking/client/CatanClient.java
@@ -147,7 +147,7 @@ public class CatanClient extends Thread{
 			else if(type == Packet.STARTGAME){
 				_isStarting = true;
 				return "Starting the game\n";
-			} else if(type == Packet.MESSAGE){
+			} else if(type == Packet.ERROR){
 				return (String) p.getObject();
 			} else
 				throw new IOException("Bad server protocol");
