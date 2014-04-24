e6736651e8471e6eb46f5a5a1243211e79ff1043
sbreslow <samuel_breslow@brown.edu>
2014-04-24 14:23:21 -0400

    chatbox functional minus bold/italic

--- src/edu/brown/cs032/atreil/catan/chat/server/ClientPool.java
+++ src/edu/brown/cs032/atreil/catan/chat/server/ClientPool.java
@@ -76,7 +76,7 @@ class ClientPool {
 			}
 			message  = sb.toString().trim();
 			_clients.get(player).send(String.format("%s (%s): %s", sender, color, message));
-			if(sender!=null && color!=null && message!=null)
+			if(!sender.equals("server"))
 				_clients.get(sender).send(String.format("%s (%s): %s", sender, color, message));
 			System.out.println("Sending " + message);
 		}
