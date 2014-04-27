6ff41986cac2c1d137c561248d07bb219acf2bd5
sbreslow <samuel_breslow@brown.edu>
2014-04-27 15:40:50 -0400

    roadbuilder stuff

--- src/edu/brown/cs032/atreil/catan/networking/server/CatanServer.java
+++ src/edu/brown/cs032/atreil/catan/networking/server/CatanServer.java
@@ -471,6 +471,6 @@ public class CatanServer extends Thread{
 	}
 	
 	public void sendRB(String name){
-		_pool.send(name, new Packet(Packet.BAD_RB, null, 0);
+		_pool.send(name, new Packet(Packet.BAD_RB, null, 0));
 	}
 }
