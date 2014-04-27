f4a86d6018675fdca13bd36ac968d60c72fdf336
Thomas Mercurio <thomas_mercurio@brown.edu>
2014-04-27 14:40:14 -0400

    Added dev card images.

--- src/edu/brown/cs032/sbreslow/catan/gui/board/BoardImages.java
+++ src/edu/brown/cs032/sbreslow/catan/gui/board/BoardImages.java
@@ -69,4 +69,12 @@ public class BoardImages {
     public static class Misc {
         public static final Image arrow = new ImageIcon("images/misc/arrow.png").getImage();
     }
+    
+    public static class DevCard {
+        public static final Image knight = new ImageIcon("images/dev_cards/knight.png").getImage();
+        public static final Image monopoly = new ImageIcon("images/dev_cards/monopoly.png").getImage();
+        public static final Image roadBuilder = new ImageIcon("images/dev_cards/road_builder.png").getImage();
+        public static final Image yearOfPlenty = new ImageIcon("images/dev_cards/year_of_plenty.png").getImage();
+        public static final Image victoryPoint = new ImageIcon("images/dev_cards/victory_point.png").getImage();
+    }
 }
