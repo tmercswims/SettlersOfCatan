15aa13405effc98bf2058c30eadfb41215e55251
sbreslow <samuel_breslow@brown.edu>
2014-04-28 17:09:48 -0400

    added chatpanel

---
+++ src/edu/brown/cs032/eheimark/catan/gui/ChatPanel.java
@@ -0,0 +1,130 @@
+package edu.brown.cs032.eheimark.catan.gui;
+
+import static edu.brown.cs032.sbreslow.catan.gui.board.BoardImages.Edge.blue;
+import static edu.brown.cs032.sbreslow.catan.gui.board.BoardImages.Edge.orange;
+import static edu.brown.cs032.sbreslow.catan.gui.board.BoardImages.Edge.red;
+import static edu.brown.cs032.sbreslow.catan.gui.board.BoardImages.Edge.white;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.SimpleAttributeSet;
+
+import edu.brown.cs032.atreil.catan.networking.client.CatanClient;
+
+public class ChatPanel extends JPanel {
+	
+	private JTextField _field;
+	private JTextPane _area;
+	private JButton _send;
+	public JPanel _panel;
+	private JScrollPane _scroll;
+	private CatanClient _client;
+	SimpleAttributeSet _red;
+	SimpleAttributeSet _blue;
+	SimpleAttributeSet _orange;
+	SimpleAttributeSet _server;
+	SimpleAttributeSet _white;
+	
+	public ChatPanel(CatanClient cc){
+		_client = cc;
+		Dimension d = new Dimension(400,600);
+		this.setPreferredSize(d);
+		_field = new JTextField(30);
+		_field.addKeyListener(new ChatListener());
+		_area = new JTextPane();
+		Dimension size = new Dimension(380,540);
+		_area.setBackground(Color.black);
+		_area.setMaximumSize(size);
+		_area.setPreferredSize(size);
+        DefaultCaret caret = (DefaultCaret)_area.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        _area.setCaret(caret);
+		_area.setEditable(false);
+        Border bl = BorderFactory.createLineBorder(Color.black);
+        TitledBorder b = BorderFactory.createTitledBorder(bl,"Chat/Log");
+        b.setTitleJustification(TitledBorder.CENTER);
+        this.setBorder(b);
+        _area.setBorder(bl);
+		_send = new JButton("Send");
+		_send.addActionListener(new SendListener());
+		_scroll = new JScrollPane(_area);
+		_scroll.setPreferredSize(size);
+		this.add(_scroll);
+		this.add(_field);
+	}
+	
+	public void addMessage(String message){
+		
+	}
+	
+	private void println(String message){
+		
+	}
+	
+	private class SendListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String message = _field.getText();
+			_field.setText("");
+			println(message);
+		}
+
+	}
+	
+	private class ChatListener implements KeyListener {
+		//red blue orange white
+		@Override
+		public void keyTyped(KeyEvent e) {
+			//System.out.println("getKeyChar "+e.getKeyChar());
+			if(e.getKeyChar() == '\n') {
+				String message = _field.getText();
+				_field.setText("");
+                //_history.addFirst(message);
+                //_unsentContents = "";
+				if(_client.getPlayer().getColor().equals(red)){
+					println("red "+message);
+				}
+				else if(_client.getPlayer().getColor().equals(blue)){
+					println("blue "+message);
+				}
+				else if(_client.getPlayer().getColor().equals(orange)){
+					println("orange "+message);
+				}
+				else if(_client.getPlayer().getColor().equals(white)){
+					println("white "+message);
+				}
+				else{
+					System.out.println(_client.getPlayer().getColor());
+				}
+			}
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+}
