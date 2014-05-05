ae624f7939e8f654eac85de0c441835953b98447
Thomas Mercurio <thomas_mercurio@brown.edu>
2014-05-04 21:48:01 -0400

    YoP frame. Whoops. Again OMG

--- src/edu/brown/cs032/sbreslow/catan/gui/devCards/testmain.java
+++ src/edu/brown/cs032/sbreslow/catan/gui/devCards/testmain.java
@@ -2,16 +2,16 @@ package edu.brown.cs032.sbreslow.catan.gui.devCards;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.List;
 
 import edu.brown.cs032.tmercuri.catan.logic.*;
+import javax.swing.JFrame;
 
 public class testmain {
 
 	public static void main(String[] args) {
 		//new SevenFrame(null);
 		//new RobberFrame((List<Player>)new ArrayList<Player>(),0, null);
-		new YoPFrame(null);
+		new YoPFrame(null, null);
 		Player p1 = new Player("Sam");
 		p1.setColor(Color.red);
 		Player p2 = new Player("not sam");
